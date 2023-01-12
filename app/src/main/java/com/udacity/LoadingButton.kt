7466409b1935d6f788249e92f1ba4aa0a943235e
elhady <i.elhady44@gmail.com>
2023-01-12 03:28:07 +0200

    draw Button Text

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.RectF
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.ContextCompat
@@ -15,9 +16,12 @@ class LoadingButton @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
+    private var textWidth = 0f
 
     private var buttonBackgroundColor = ContextCompat.getColor(context, R.color.colorPrimary)
     private var buttonBackgroundRect = RectF()
+    private var buttonTextColor = ContextCompat.getColor(context, R.color.white)
+    private var buttonText = "Download"
 
     private val valueAnimator = ValueAnimator()
 
@@ -38,9 +42,23 @@ class LoadingButton @JvmOverloads constructor(
         color = buttonBackgroundColor
     }
 
+    private val paintButtonText = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        color = buttonTextColor
+        textSize = 50f
+        typeface = Typeface.DEFAULT
+        textAlign = Paint.Align.LEFT
+    }
     private fun drawButtonBackground(canvas: Canvas) {
         buttonBackgroundRect.set(0f, 0f, widthSize.toFloat(), heightSize.toFloat())
         canvas.drawRect(buttonBackgroundRect, paintButtonBackground)
+    }
+    private fun drawButtonText(canvas: Canvas){
+        textWidth = paintButtonText.measureText(buttonText)
+        canvas.drawText(
+            buttonText,
+            (widthSize - textWidth) / 2f,
+            (heightSize - (paintButtonText.ascent() + paintButtonText.descent())) / 2f, paintButtonText)
     }
 
     /** onDraw() method to draw the custom view, using a Canvas object styled by a Paint object.
@@ -50,6 +68,7 @@ class LoadingButton @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         drawButtonBackground(canvas)
+        drawButtonText(canvas)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
