fe1d3560a04305b17e1fae16028fdce5265603b3
elhady <i.elhady44@gmail.com>
2023-01-25 01:20:34 +0200

    set on click listener to custom button and call download() function with selected url

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -12,19 +12,19 @@ import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import kotlinx.android.synthetic.main.content_main.view.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private var downloadID: Long = 0
 
-        private lateinit var notificationManager: NotificationManager
+    private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
@@ -38,33 +38,74 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         createChannel(getString(R.string.channel_id), getString(R.string.channel_name))
-//        custom_button.setOnClickListener {
-//            download()
-//        }
-        custom_button.setOnClickListener{
-//            notificationManager.sendDownloadCompleted("Test Notification", applicationContext)
-            custom_button.buttonState = ButtonState.Clicked
+
+
+        customButton.setOnClickListener {
+            customButton.buttonState = ButtonState.Clicked
+            val urlDownload = when (downloadGroup.checkedRadioButtonId) {
+                glideRadioButton.id -> GLIDE_URL
+                loadRadioButton.id -> LOAD_URL
+                retrofitRadioButton.id -> RETROFIT_URL
+                else -> null
+            }
+            download(urlDownload)
         }
     }
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
+            customButton.buttonState = ButtonState.Completed
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            if (downloadID == id) {
+                val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+                val query = DownloadManager.Query()
+                query.setFilterById(id)
+
+                val cursor = downloadManager.query(query)
+                if (cursor.moveToFirst()) {
+                    val status =
+                        when (cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_STATUS))) {
+                            DownloadManager.STATUS_SUCCESSFUL -> getString(R.string.success)
+                            DownloadManager.STATUS_FAILED -> getString(R.string.failed)
+                            else -> getString(R.string.unknown)
+                        }
+                    val fileName = cursor.getString(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_TITLE))
+                    val fileDescription = cursor.getString(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_DESCRIPTION))
+                    notificationManager.sendDownloadCompleted(fileName, fileDescription, status, applicationContext)
+                }
+            }
         }
     }
 
-    private fun download() {
-        val request =
-            DownloadManager.Request(Uri.parse(URL))
-                .setTitle(getString(R.string.app_name))
-                .setDescription(getString(R.string.app_description))
-                .setRequiresCharging(false)
-                .setAllowedOverMetered(true)
-                .setAllowedOverRoaming(true)
+    private fun download(urlDownload: String?) {
+        if (urlDownload != null) {
+            val request =
+                DownloadManager.Request(Uri.parse(urlDownload))
+                    .setTitle(getString(R.string.app_name))
+                    .setDescription(getString(R.string.app_description))
+                    .setRequiresCharging(false)
+                    .setAllowedOverMetered(true)
+                    .setAllowedOverRoaming(true)
 
-        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+            when (urlDownload) {
+                LOAD_URL -> {
+                    request.setTitle(getString(R.string.load))
+                    request.setDescription(getString(R.string.load_app_description))
+                }
+                GLIDE_URL -> {
+                    request.setTitle(getString(R.string.glide))
+                    request.setDescription(getString(R.string.glide_description))
+                }
+                RETROFIT_URL -> {
+                    request.setTitle(getString(R.string.retrofit))
+                    request.setDescription(getString(R.string.retrofit_description))
+                }
+            }
+
+            val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            downloadID =
+                downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+        } else Toast.makeText(applicationContext, getString(R.string.select_one), Toast.LENGTH_SHORT).show()
     }
 
     private fun createChannel(channelId: String, channelName: String) {
@@ -90,9 +131,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val URL =
+        private const val GLIDE_URL =
+            "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
+        private const val LOAD_URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
-        private const val CHANNEL_ID = "channelId"
+        private const val RETROFIT_URL =
+            "https://github.com/square/retrofit/archive/refs/heads/master.zip"
     }
 
 }
--- app/src/main/java/com/udacity/NotificationUtils.kt
+++ app/src/main/java/com/udacity/NotificationUtils.kt
@@ -4,25 +4,32 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import androidx.core.app.NotificationCompat
 
 // Notification ID.
 private val NOTIFICATION_ID = 0
 
-fun NotificationManager.sendDownloadCompleted(messageBody: String, applicationContext: Context) {
+fun NotificationManager.sendDownloadCompleted(fileName: String, fileDescription: String, status: String, applicationContext: Context) {
+    val extras = Bundle()
+    extras.putString("status", status)
+    extras.putString("fileName", fileName)
+    extras.putString("fileDescription", fileDescription)
 
     val detailIntent = Intent(applicationContext, DetailActivity::class.java)
+    detailIntent.putExtras(extras)
+
     val contentPendingIntent = PendingIntent.getActivity(applicationContext, NOTIFICATION_ID, detailIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
     // Build notification
     val builder = NotificationCompat.Builder(applicationContext, applicationContext.getString(R.string.channel_id))
         .setSmallIcon(R.drawable.ic_assistant_black_24dp)
-        .setContentTitle(messageBody)
-        .setContentText(applicationContext.getString(R.string.app_description))
+        .setContentTitle(fileName)
+        .setContentText(fileDescription)
         .setContentIntent(contentPendingIntent)
         .setAutoCancel(true)
         .setPriority(NotificationCompat.PRIORITY_HIGH)
-        .addAction(R.drawable.ic_assistant_black_24dp, applicationContext.getString(R.string.notification_button), contentPendingIntent)
+        .addAction(R.drawable.ic_assistant_black_24dp, applicationContext.getString(R.string.action_button), contentPendingIntent)
 
     notify(NOTIFICATION_ID, builder.build())
 
